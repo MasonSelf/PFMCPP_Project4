3e13e2c5cb99532c6252b3aafb26dc4b96f56271
MasonSelf <masonselfmusic@gmail.com>
2020-12-11 20:57:51 +0000

    make spacing consistent

--- main.cpp
+++ main.cpp
@@ -495,43 +495,34 @@ void part3()
     IntType it ( 34 );
     DoubleType pi( 3.14 );
 
-    //replace ft.multiply( ft ).multiply( ft ).divide( static_cast<float>(it) )
-    ft *= ft *= ft /= static_cast<float>(it);
-
+    ft *= ft;
+    ft *= ft;
+    ft /= static_cast<float>(it);
     std::cout << "The result of FloatType^3 divided by IntType is: " << ft << std::endl;
 
-    //replace dt.multiply( 3 ).add( it )
     dt*= 3;
     dt += static_cast<double>(it);
-
     std::cout << "The result of DoubleType times 3 plus IntType is : " << dt << std::endl;
     
-    //replace it.divide( static_cast<int>(pi) ).multiply( static_cast<int>(dt) ).subtract( static_cast<int>(ft) )
     it /= static_cast<int>(pi);
     it *= static_cast<int>(dt);
     it -= static_cast<int>(ft);
-
     std::cout << "The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: " << it << std::endl;
     std::cout << "An operation followed by attempts to divide by 0, which are ignored and warns user: " << std::endl;
 
-    //replace it.multiply(it).divide(0).divide(0.0f).divide(0.0)
     it *= it;
     it /= 0;
     it /= static_cast<int>(0.f);
     it /= static_cast<int>(0.f);
-
     std::cout << it << std::endl;
     
-    //replace it.multiply( static_cast<int>(ft) )
     it *= static_cast<int>(ft);
-
     std::cout << "FloatType x IntType  =  " << it << std::endl;
 
-    //replace it.add( static_cast<int>(dt) ).add( static_cast<int>(ft) ).multiply( 24 )
+
     it += static_cast<int>(dt);
     it += static_cast<int>(ft);
     it *= 24;
-
     std::cout << "(IntType + DoubleType + FloatType) x 24 = " << it << std::endl;
 }
 
